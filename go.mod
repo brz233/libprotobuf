f7e6e06699e32b780be629c738d941cc5fe44da5
chenhualin <chen1995+>
2020-02-14 16:00:15 +0800

    other

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/brz233/libprotobuf
 
 go 1.13
 
-require github.com/golang/protobuf v1.3.3 // indirect
+require (
+	github.com/go-playground/assert v1.2.1
+	github.com/golang/protobuf v1.3.3
+)
---
+++ v1/message_test.go
@@ -0,0 +1,39 @@
+package v1
+
+import (
+	"fmt"
+	pb "github.com/brz233/libprotobuf/v1/protocol"
+	"github.com/go-playground/assert"
+	"github.com/golang/protobuf/proto"
+	"testing"
+)
+
+func TestRecord_Bytes(t *testing.T) {
+	obj := Record{1: String("abc"), 2: Sint32(-56)}
+	bs, err := obj.Bytes()
+	if err != nil {
+		t.Error(err)
+	}
+	fmt.Println(bs)
+}
+
+func TestDouble_Bytes(t *testing.T) {
+	record := Record{1: Double(9.7)}
+	bs, err := record.Bytes()
+	if err != nil {
+		t.Error(err)
+	}
+	assert.Equal(t, err, nil)
+
+	test1 := pb.Test1{}
+	err = proto.Unmarshal(bs, &test1)
+	if err != nil {
+		t.Error(err)
+	}
+	assert.Equal(t, err, nil)
+
+	assert.Equal(t, test1.DoubleNumber, 9.7)
+	if !(test1.DoubleNumber-9.7 < 10e-7) {
+		t.Error(err, test1.DoubleNumber)
+	}
+}
